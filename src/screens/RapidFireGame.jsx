b7232e7123159e368246839c1cd95b6d97ca4fde
Shlok Jhawar <shlokj@gmail.com>
2024-06-04 23:45:55 -0700

    fetch reflection documents

--- src/screens/RapidFireGame.jsx
+++ src/screens/RapidFireGame.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
-import { Container } from '@mui/material';
-import Header from '../components/Header';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import RapidFireButtonComponents from '../components/RapidFireButtonComponents';
-import RapidFireQuestionComponent from '../components/RapidFireQuestionComponent';
+import React, { useState } from "react";
+import { Container } from "@mui/material";
+import Header from "../components/Header";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import RapidFireButtonComponents from "../components/RapidFireButtonComponents";
+import RapidFireQuestionComponent from "../components/RapidFireQuestionComponent";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function RapidFireGame() {
+  const [documents, setDocuments] = useState([]);
+
+  const handleFetchCollection = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "reflections"));
+      const docsArray = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setDocuments(docsArray);
+      console.log("Collection fetched successfully");
+      console.log(docsArray);
+    } catch (e) {
+      console.error("Error fetching collection: ", e);
+    }
+  };
+
   return (
     <Container>
-      <Header isLoggedIn={true} userName='<username>' />
-      <Box>
+      <Header isLoggedIn={true} userName="<username>" />
+      <Box onClick={handleFetchCollection}>
+        {/* TODO: remove the onClick, this is only to test  */}
         <Grid container marginTop={10}>
           <Grid item xs={8}>
             <RapidFireQuestionComponent />
