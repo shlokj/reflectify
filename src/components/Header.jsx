bd84a0c7557afe89883280240f6850d609ac4eb4
Shlok Jhawar <shlokj@gmail.com>
2024-06-05 20:42:04 -0700

    fix logout for all pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Typography } from '@mui/material';
-import LogoutIcon from '@mui/icons-material/Logout';
-import { onAuthStateChanged, signOut } from 'firebase/auth';
-import { auth } from '../firebase';
-import { useNavigate } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router";
 
 export default function Header({ isLoggedIn }) {
   const navigate = useNavigate();
@@ -13,10 +13,10 @@ export default function Header({ isLoggedIn }) {
   const handleLogout = () => {
     signOut(auth)
       .then(() => {
-        console.log('User signed out');
+        console.log("User signed out");
       })
       .catch((error) => {
-        console.error('Error signing out:', error);
+        console.error("Error signing out:", error);
       });
   };
 
@@ -25,6 +25,8 @@ export default function Header({ isLoggedIn }) {
       if (currentUser) {
         setUser(currentUser.displayName);
         console.log("the user's name is " + currentUser.displayName);
+      } else {
+        navigate("/");
       }
     });
 
@@ -33,77 +35,77 @@ export default function Header({ isLoggedIn }) {
 
   return (
     <Box
-      display='flex'
-      flexDirection='column'
-      justifyContent='space-between'
-      alignItems='center'
-      width='100vw'
-      position='fixed'
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+      alignItems="center"
+      width="100vw"
+      position="fixed"
       top={0}
       left={0}
       zIndex={1000}
-      bgcolor='#E5F1F7'
+      bgcolor="#E5F1F7"
       p={1}
     >
       <Box
-        display='flex'
-        alignItems='center'
-        justifyContent='space-between'
-        width='100%'
-        padding='10px 200px 0px 200px'
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        width="100%"
+        padding="10px 200px 0px 200px"
       >
-        <Box display='flex' alignItems='center' ml={2}>
+        <Box display="flex" alignItems="center" ml={2}>
           <img
             src={`${process.env.PUBLIC_URL}/logo.png`}
-            alt='React Logo'
+            alt="React Logo"
             width={100}
-            style={{ marginRight: '10px' }}
+            style={{ marginRight: "10px" }}
           />
           <Typography
-            variant='h2'
-            component='h1'
-            sx={{ fontWeight: 'bold', letterSpacing: '0.1em' }}
+            variant="h2"
+            component="h1"
+            sx={{ fontWeight: "bold", letterSpacing: "0.1em" }}
           >
             REFLECTIFY
           </Typography>
         </Box>
-        <Box display='flex' alignItems='center' mr={6}>
+        <Box display="flex" alignItems="center" mr={6}>
           {isLoggedIn ? (
             <>
               <Typography
-                variant='h5'
-                component='p'
-                sx={{ fontWeight: 'bold', marginRight: '10px' }}
+                variant="h5"
+                component="p"
+                sx={{ fontWeight: "bold", marginRight: "10px" }}
               >
-                {!!user ? 'Welcome, ' + user : 'Welcome'}
+                {!!user ? "Welcome, " + user : "Welcome"}
               </Typography>
               <LogoutIcon sx={{ fontSize: 60 }} onClick={handleLogout} />
             </>
           ) : (
             <Box
-              display='flex'
-              flexDirection='column'
-              alignItems='flex-end'
-              sx={{ textAlign: 'right' }}
+              display="flex"
+              flexDirection="column"
+              alignItems="flex-end"
+              sx={{ textAlign: "right" }}
             >
               <Typography
-                variant='h5'
-                component='p'
-                sx={{ fontWeight: 'bold' }}
+                variant="h5"
+                component="p"
+                sx={{ fontWeight: "bold" }}
               >
                 REFLECT
               </Typography>
               <Typography
-                variant='h5'
-                component='p'
-                sx={{ fontWeight: 'bold' }}
+                variant="h5"
+                component="p"
+                sx={{ fontWeight: "bold" }}
               >
                 ENGAGE
               </Typography>
               <Typography
-                variant='h5'
-                component='p'
-                sx={{ fontWeight: 'bold' }}
+                variant="h5"
+                component="p"
+                sx={{ fontWeight: "bold" }}
               >
                 ENHANCE
               </Typography>
